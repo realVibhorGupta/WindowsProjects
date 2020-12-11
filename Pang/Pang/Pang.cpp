79bc28945e2a16fcd1a8bbe6b4546ebec0e9da1d
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2020-12-11 11:25:48 +0530

    Add files via upload

---
+++ Pang/Pang/Pang.cpp
@@ -0,0 +1,12 @@
+// Pang.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include   <iostream>
+#include  <conio.h>
+#include<mmstream.h>
+int main()
+{
+    return 0;
+}
+
